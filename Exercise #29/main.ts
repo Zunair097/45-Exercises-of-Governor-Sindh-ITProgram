e9991de7a1ca744c6d0b037c1a42ba32b4538896
Zunair097 <161992162+Zunair097@users.noreply.github.com>
2024-03-09 12:26:40 +0500

    45 Exercises for Governor Sindh IT Course

---
+++ Exercise #29/main.ts
@@ -0,0 +1,33 @@
+// Creating Array
+let fvt_Fruits = ["Mango", "Strawberry", "Orange"];
+
+
+// Using Five Independent If Statement
+
+if(fvt_Fruits.includes("Mango")){
+    console.log("I really like Mangoes.");
+}
+if(fvt_Fruits.includes("Strawberry")){
+    console.log("I really like strawberries.");
+}
+if(fvt_Fruits.includes("Apple")){
+    console.log("I really like Apple");
+}
+if(fvt_Fruits.includes("Orange")){
+    console.log("I really like Orange.");
+}
+if(fvt_Fruits.includes("Banana")){
+    console.log("I really like Banana");
+}
+
+
+
+
+
+
+
+
+
+
+
+
